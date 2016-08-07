bbeb83009ffcfb3c00ad53358bf231aa846274cb
simonguo <simonguo.2009@gmail.com>
2016-08-07 14:19:22 +0800

    Add ArrayType.js

---
+++ src/ArrayType.js
@@ -0,0 +1,64 @@
+import Type from './Type';
+
+class ArrayType extends Type {
+    static from(n) {
+        return n;
+    }
+
+    constructor(errorMessage = 'no error message') {
+        super('array');
+        super.addValidator(v => Array.isArray(v), errorMessage);
+    }
+
+    rangeLength(minLength, maxLength, errorMessage) {
+        super.addValidator(value => value.length >= minLength && value.length <= maxLength, errorMessage);
+        return this;
+    }
+
+    minLength(minLength, errorMessage) {
+        super.addValidator(value => value.length >= minLength, errorMessage);
+        return this;
+    }
+
+    maxLength(maxLength, errorMessage) {
+        super.addValidator(value => value.length <= maxLength, errorMessage);
+        return this;
+    }
+
+    unrepeatable(errorMessage) {
+        super.addValidator((items) => {
+            let hash = {};
+            for (let i in items) {
+                if (hash[items[i]]) {
+                    return false;
+                }
+                hash[items[i]] = true;
+            }
+            return true;
+        }, errorMessage);
+        return this;
+    }
+
+    /**
+     * @example
+     * ArrayType('这是一个数组').shape(
+     *      StringType().isOneOf(['数码','体育','游戏','旅途','其他'],
+     *      '只能是选择中的值'
+     * )
+     */
+    shape(type, errorMessage) {
+
+        super.addValidator((items) => {
+            let valids = items.map((value) => {
+                return type.check(value)
+            });
+            return valids.reduce((previousValue, currentValue) => {
+                return !previousValue.hasError && !currentValue.hasError
+            })
+        }, errorMessage);
+
+        return this;
+    }
+}
+
+export default (errorMessage) => new ArrayType(errorMessage);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export * from './Schema';
 export StringType from './StringType';
 export NumberType from './NumberType';
+export ArrayType from './ArrayType';
