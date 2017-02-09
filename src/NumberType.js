04eb5de05e6eaf3d46b20cd82b5057eeafc7ded0
simonguo <simonguo.2009@gmail.com>
2017-02-09 15:54:39 +0800

    Added support decimal for NumberType

--- src/NumberType.js
+++ src/NumberType.js
@@ -1,13 +1,21 @@
 import Type from './Type';
 
+function _f(value) {
+    return +value;
+}
+
 class NumberType extends Type {
     static from(n) {
-        return +n;
+        return n;
     }
 
     constructor(errorMessage = 'no error message') {
         super('number');
-        super.addValidator(value => /^[0-9]+.?[0-9]*$/.test(value), errorMessage);
+        super.addValidator(value => /^-?(?:\d+|\d{1,3}(?:,\d{3})+)(?:\.\d+)?$/.test(value), errorMessage);
+    }
+
+    isInteger(){
+         super.addValidator(value => /^-?\d+$/.test(value), errorMessage);
     }
 
     pattern(regexp, errorMessage) {
@@ -16,22 +24,22 @@ class NumberType extends Type {
     }
 
     isOneOf(numLst, errorMessage) {
-        super.addValidator(value => value in numLst, errorMessage);
+        super.addValidator(value => _f(value) in numLst, errorMessage);
         return this;
     }
 
     range(min, max, errorMessage) {
-        super.addValidator(value => value >= min && value <= max, errorMessage);
+        super.addValidator(value => _f(value) >= min && _f(value) <= max, errorMessage);
         return this;
     }
 
     min(min, errorMessage) {
-        super.addValidator(value => value >= min, errorMessage);
+        super.addValidator(value => _f(value) >= min, errorMessage);
         return this;
     }
 
     max(max, errorMessage) {
-        super.addValidator(value => value <= max, errorMessage);
+        super.addValidator(value => _f(value) <= max, errorMessage);
         return this;
     }
 }
--- test/index.js
+++ test/index.js
@@ -1,26 +1,40 @@
 var should = require('chai').should();
 var schema = require('../lib');
-var schemaBuilder = schema.SchemaBuilder;
+var Schema = schema.Schema;
 var stringType = schema.StringType;
 var numberType = schema.NumberType;
 
-describe('#SchemaBuilder', () => {
+describe('#Schema', () => {
     it('save Schema as proporty', () => {
         let schemaData = { data: stringType() };
-        let schema = schemaBuilder(schemaData);
+        let schema = new Schema(schemaData);
         schema.schema.should.equal(schemaData);
     });
 
+
     it('get field value type of given field name', () => {
         let schemaData = { data: numberType() };
-        let schema = schemaBuilder(schemaData);
+        let schema = new Schema(schemaData);
         schema.getFieldType('data').should.equal(schemaData.data);
     });
 
     it('should return error information', () => {
         let schemaData = { data: numberType() };
-        let schema = schemaBuilder(schemaData);
-        let checkResult = schema.checkForField('data', 233);
-        checkResult.should.have.property('err').be.a('boolean');
+        let schema = new Schema(schemaData);
+        let checkResult = schema.checkForField('data', "2.22");
+
+        checkResult.should.have.property('hasError').be.a('boolean');
     });
+
+
+    it('should valid NumberType ', () => {
+        let schemaData = { data: numberType() };
+        let schema = new Schema(schemaData);
+
+        schema.checkForField('data', "2.22").hasError.should.equal(false);
+        schema.checkForField('data', 2.22).hasError.should.equal(false);
+        schema.checkForField('data', -222).hasError.should.equal(false);
+
+    });
+
 });
