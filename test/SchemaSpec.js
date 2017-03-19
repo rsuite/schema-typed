baa314f2d7f661b49afc92f9ea7ce922700b33a2
simonguo <simonguo.2009@gmail.com>
2017-03-19 15:41:50 +0800

    add test case

--- test/SchemaSpec.js
+++ test/SchemaSpec.js
@@ -1,7 +1,7 @@
 const should = require('chai').should();
 const schema = require('../src');
 
-const { StringType, NumberType, Schema } = schema;
+const { StringType, NumberType, Schema, SchemaModel } = schema;
 
 describe('#Schema', () => {
 
@@ -25,6 +25,27 @@ describe('#Schema', () => {
 
         checkResult.should.have.property('hasError').be.a('boolean');
     });
+
+
+    it('should return error information', () => {
+
+        const userModel = SchemaModel({
+            username: StringType().isRequired('用户名不能为空'),
+            email: StringType().isEmail('请输入正确的邮箱'),
+            age: NumberType('年龄应该是一个数字').range(18, 30, '年应该在 18 到 30 岁')
+        });
+
+        const checkStatus = userModel.check({
+            username: 'foobar',
+            email: 'foo@bar.com',
+            age: 40
+        });
+
+        console.log(checkStatus);
+
+    });
+
+
 
 
 
