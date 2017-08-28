f4d1ecf89f0ce458843f6735f87eb1c44db15c71
simonguo <simonguo.2009@gmail.com>
2017-08-28 16:43:35 +0800

    Update test case

--- test/ArrayTypeSpec.js
+++ test/ArrayTypeSpec.js
@@ -4,14 +4,15 @@ const { ArrayType, StringType, Schema } = schema;
 
 describe('#ArrayType', () => {
 
-    let schemaData1 = { data: ArrayType().shape(StringType().isEmail('应该是一个 email'), '格式错误') };
-    let schemaData2 = { data: ArrayType().shape(StringType().isEmail(), '格式错误') };
+    let schemaData1 = { data: ArrayType().of(StringType().isEmail('应该是一个 email'), '格式错误') };
+    let schemaData2 = { data: ArrayType().of(StringType().isEmail(), '格式错误') };
 
 
     it('Should be a valid array', () => {
 
         let schema = new Schema(schemaData1);
         let schema2 = new Schema(schemaData2);
+
         schema.checkForField('data', ['simon.guo@hypers.com', 'ddddd@d.com', 'ddd@bbb.com']).hasError.should.equal(false);
         let checkStatus = schema.checkForField('data', ['simon.guo@hypers.com', 'ddddd', 'ddd@bbb.com']);
 
---
+++ test/ObjectTypeSpec.js
@@ -0,0 +1,27 @@
+const should = require('chai').should();
+const schema = require('../src');
+const { ObjectType, StringType, NumberType, Schema } = schema;
+
+describe('#ObjectType', () => {
+
+    let schemaData1 = {
+        data: ObjectType().shape({
+            email: StringType().isEmail('应该是一个 email'),
+            age: NumberType().min(18, '年龄应该大于18岁'),
+        })
+    };
+
+    it('Should be a valid object', () => {
+
+        let schema = new Schema(schemaData1);
+        schema.checkForField('data', { email: 'simon.guo@hypers.com', age: 19 }).hasError.should.equal(false);
+        schema.checkForField('data', { email: 'simon.guo', age: 19 }).hasError.should.equal(true);
+
+        let checkStatus = schema.checkForField('data', { email: 'simon.guo@hypers.com', age: 17 });
+
+        checkStatus.hasError.should.equal(true);
+        checkStatus.errorMessage.should.equal('年龄应该大于18岁');
+
+    });
+
+});
