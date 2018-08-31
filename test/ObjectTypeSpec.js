6f2d19869de840f895e05c9e6f3c953de77ee4a2
simonguo <simon.guo@hypers.com>
2018-08-31 14:12:42 +0800

    Update tests

--- test/ObjectTypeSpec.js
+++ test/ObjectTypeSpec.js
@@ -1,17 +1,19 @@
+import { flaser } from 'object-flaser';
+
 const should = require('chai').should();
 const schema = require('../src');
 const { ObjectType, StringType, NumberType, Schema } = schema;
 
 describe('#ObjectType', () => {
-  let schemaData1 = {
-    data: ObjectType().shape({
-      email: StringType().isEmail('应该是一个 email'),
-      age: NumberType().min(18, '年龄应该大于18岁')
-    })
-  };
-
   it('Should be a valid object', () => {
-    let schema = new Schema(schemaData1);
+    let schemaData = {
+      data: ObjectType().shape({
+        email: StringType().isEmail('应该是一个 email'),
+        age: NumberType().min(18, '年龄应该大于18岁')
+      })
+    };
+
+    let schema = new Schema(schemaData);
     schema
       .checkForField('data', { email: 'simon.guo@hypers.com', age: 19 })
       .hasError.should.equal(false);
@@ -21,5 +23,22 @@ describe('#ObjectType', () => {
 
     checkStatus.hasError.should.equal(true);
     checkStatus.errorMessage.should.equal('年龄应该大于18岁');
+  });
+
+  it('Should be a valid object by flaser', () => {
+    let schemaData = {
+      'data.email': StringType().isEmail('应该是一个 email'),
+      'data.age': NumberType().min(18, '年龄应该大于18岁')
+    };
+
+    let data = {
+      data: { email: 'simon.guo@hypers.com', age: 17 }
+    };
+
+    let schema = new Schema(schemaData);
+    let checkStatus = schema.check(flaser(data));
+
+    checkStatus['data.email'].hasError.should.equal(false);
+    checkStatus['data.age'].hasError.should.equal(true);
   });
 });
