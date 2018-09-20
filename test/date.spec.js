ca2c84c88775b102e0c327d9930a7da17f8259b5
blackcater <blackcater2015@gmail.com>
2018-09-20 14:28:56 +0800

    add test cases for date file

--- test/date.spec.js
+++ test/date.spec.js
@@ -1,7 +1,175 @@
+import DateType from '../src/DateType';
+
 describe('@DateType', () => {
   describe('#constructor', () => {
-    it('should be a valid date', () => {
-      expect(1).toBe(1);
+    it('should be initialized correctly', done => {
+      const type = new DateType('it should be a date value');
+
+      expect(type.name).toBe('date');
+      expect(type.required).toBe(false);
+      expect(type.requiredMessage).toBeFalsy();
+      expect(type.rules).toHaveLength(1);
+
+      type.check(undefined, undefined, result => {
+        expect(result).toMatchObject({ hasError: false });
+      });
+
+      type.check(null, undefined, result => {
+        expect(result).toMatchObject({ hasError: false });
+      });
+
+      type.check('', undefined, result => {
+        expect(result).toMatchObject({ hasError: false });
+      });
+
+      type.check(new Date(), undefined, result => {
+        expect(result).toMatchObject({ hasError: false });
+      });
+
+      type.check([], undefined, result => {
+        expect(result).toMatchObject({
+          hasError: true,
+          errorMessage: 'it should be a date value'
+        });
+      });
+
+      setTimeout(() => done(), 250);
+    });
+  });
+
+  describe('#range', () => {
+    it('should be correct', done => {
+      const type = new DateType('it should be a date value');
+      const now = new Date();
+
+      expect(type.rules).toHaveLength(1);
+
+      type.range(
+        new Date(now).setDate(now.getDate() - 1),
+        new Date(now).setDate(now.getDate() + 1),
+        'it should be later than yesterday and earlier than tomorrow'
+      );
+
+      expect(type.rules).toHaveLength(2);
+
+      type.check(undefined, undefined, result => {
+        expect(result).toMatchObject({ hasError: false });
+      });
+
+      type.check(null, undefined, result => {
+        expect(result).toMatchObject({ hasError: false });
+      });
+
+      type.check('', undefined, result => {
+        expect(result).toMatchObject({ hasError: false });
+      });
+
+      type.check(new Date(), undefined, result => {
+        expect(result).toMatchObject({ hasError: false });
+      });
+
+      type.check(new Date(now).setDate(now.getDate() - 2), undefined, result => {
+        expect(result).toMatchObject({ hasError: true, errorMessage: 'it should be later than yesterday and earlier than tomorrow' });
+      });
+
+      type.check([], undefined, result => {
+        expect(result).toMatchObject({
+          hasError: true,
+          errorMessage: 'it should be a date value'
+        });
+      });
+
+      setTimeout(() => done(), 250);
+    });
+  });
+
+  describe('#min', () => {
+    it('should be correct', done => {
+      const type = new DateType('it should be a date value');
+      const now = new Date();
+
+      expect(type.rules).toHaveLength(1);
+
+      type.min(
+        new Date(now).setDate(now.getDate() - 1),
+        'it should be later than yesterday'
+      );
+
+      expect(type.rules).toHaveLength(2);
+
+      type.check(undefined, undefined, result => {
+        expect(result).toMatchObject({ hasError: false });
+      });
+
+      type.check(null, undefined, result => {
+        expect(result).toMatchObject({ hasError: false });
+      });
+
+      type.check('', undefined, result => {
+        expect(result).toMatchObject({ hasError: false });
+      });
+
+      type.check(new Date(), undefined, result => {
+        expect(result).toMatchObject({ hasError: false });
+      });
+
+      type.check(new Date(now).setDate(now.getDate() - 2), undefined, result => {
+        expect(result).toMatchObject({ hasError: true, errorMessage: 'it should be later than yesterday' });
+      });
+
+      type.check([], undefined, result => {
+        expect(result).toMatchObject({
+          hasError: true,
+          errorMessage: 'it should be a date value'
+        });
+      });
+
+      setTimeout(() => done(), 250);
+    });
+  });
+
+  describe('#max', () => {
+    it('should be correct', done => {
+      const type = new DateType('it should be a date value');
+      const now = new Date();
+
+      expect(type.rules).toHaveLength(1);
+
+      type.max(
+        new Date(now).setDate(now.getDate() + 1),
+        'it should be earlier than tomorrow'
+      );
+
+      expect(type.rules).toHaveLength(2);
+
+      type.check(undefined, undefined, result => {
+        expect(result).toMatchObject({ hasError: false });
+      });
+
+      type.check(null, undefined, result => {
+        expect(result).toMatchObject({ hasError: false });
+      });
+
+      type.check('', undefined, result => {
+        expect(result).toMatchObject({ hasError: false });
+      });
+
+      type.check(new Date(), undefined, result => {
+        expect(result).toMatchObject({ hasError: false });
+      });
+
+      type.check(new Date(now).setDate(now.getDate() + 2), undefined, result => {
+        expect(result).toMatchObject({ hasError: true, errorMessage: 'it should be earlier than tomorrow' });
+      });
+
+      type.check([], undefined, result => {
+        expect(result).toMatchObject({
+          hasError: true,
+          errorMessage: 'it should be a date value'
+        });
+      });
+
+      setTimeout(() => done(), 250);
     });
   });
 });
