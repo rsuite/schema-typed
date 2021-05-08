cf0f10bae3ea5d7bcdc62878bdfd5d5ebbd2184e
Simon Guo <simonguo.2009@gmail.com>
2021-05-08 18:38:20 +0800

    fix(ts): add export to type declaration (#30)

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,15 @@ import { default as DateType } from './DateType';
 import { default as ObjectType } from './ObjectType';
 import { default as BooleanType } from './BooleanType';
 
+export type {
+  CheckResult,
+  SchemaCheckResult,
+  SchemaDeclaration,
+  CheckType,
+  RuleType,
+  ValidCallbackType
+} from './types';
+
 export {
   SchemaModel,
   Schema,
@@ -17,17 +26,4 @@ export {
   DateType,
   ObjectType,
   BooleanType
-};
-
-export default {
-  Model: SchemaModel,
-  Types: {
-    MixedType,
-    StringType,
-    NumberType,
-    ArrayType,
-    DateType,
-    ObjectType,
-    BooleanType
-  }
 };
